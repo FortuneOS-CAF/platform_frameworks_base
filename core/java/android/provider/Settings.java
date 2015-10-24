14101055cec30a303bee16b28ed085a3b462579d
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-10-23 23:44:43 -0400

    !fixup Hide power menu on secure lockscreen [1/2]
    Change-Id: I07255a21a11e8a7ec3229b1d286b5d686b8d054a
    Signed-off-by: DarkJoker360 <simoespo159@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5825,6 +5825,12 @@ public final class Settings {
         public static final String NOTIFICATION_SOUND_VIB_SCREEN_ON = "notification_sound_vib_screen_on";
 
         /**
+         * Whether to show power menu on LockScreen
+         * @hide
+         */
+        public static final String LOCKSCREEN_ENABLE_POWER_MENU = "lockscreen_enable_power_menu";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
@@ -11525,13 +11531,6 @@ public final class Settings {
          * @hide
          */
         public static final String SYSTEM_BLACK_THEME = "system_black_theme";
-
-        /**
-         * Disable power menu on secure lock screens
-         *
-         * @hide
-         */
-        public static final String POWER_MENU_HIDE_ON_SECURE = "power_menu_hide_on_secure";
 
         /**
          * Keys we no longer back up under the current schema, but want to continue to
